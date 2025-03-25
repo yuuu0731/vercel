f2c97e1827710816e9360a62d7d3d202a9840ea2
yuuu0731 <beo5980@gmail.com>
2025-03-25 11:49:56 +0800

    v1(時間+8)

--- index.py
+++ index.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template,request
-from datetime import datetime
-
+from datetime import datetime, timezone, timedelta
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>楊子青Python網頁</h1>"
+    homepage = "<h1>楊子青Python網頁(時間+8)</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=tcyang&work=pu>傳送使用者暱稱</a><br>"
@@ -19,7 +18,8 @@ def course():
     
 @app.route("/today")
 def today():
-    now = datetime.now()
+    tz = timezone(timedelta(hours=+8))
+    now = datetime.now(tz)
     return render_template("today.html", datetime = str(now))
 
 @app.route("/about")
